bdb6387047e0e8fa344e7fe4c804f2c57aa2e8e1
Lucas Bittencourt <lukas.gabriel.2012lks@gmail.com>
2024-08-05 10:33:49 -0300

    configuração do angular.json, criação no polyfills e importação do zone.ts

---
+++ formulario-angular/src/polyfills.ts
@@ -0,0 +1,14 @@
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// Este arquivo pode ser usado para importar polyfills adicionais necessários para a aplicação.
+
+// Zone JS é necessário para Angular. Inclua sempre o zone.js ao usar Angular.
+import 'zone.js';  // Incluído com o Angular CLI.
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+
+// Se necessário, você pode adicionar outros polyfills ou importações específicas de sua aplicação aqui.
