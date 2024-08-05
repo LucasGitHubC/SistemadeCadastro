a9eb992b893131bbd67c9ebe4e80bc27141953f3
Lucas Bittencourt <lukas.gabriel.2012lks@gmail.com>
2024-08-05 13:14:34 -0300

    Criação janela confirma cadastro

--- formulario-angular/src/app/cadastro/cadastro.component.ts
+++ formulario-angular/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-cadastro',
   standalone: true, // Define o componente como standalone
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css'],
-  imports: [FormsModule] // Importa FormsModule para o componente standalone
+  imports: [FormsModule, CommonModule] // Adiciona CommonModule aqui
 })
 export class CadastroComponent {
   cadastro = {
@@ -19,11 +20,21 @@ export class CadastroComponent {
     gender: ''
   };
 
+  showModal: boolean = false; // Controle de exibição do modal
+  modalMessage: string = ''; // Mensagem do modal
+
   onSubmit(form: any) {
     if (form.valid) {
+      this.modalMessage = 'Cadastro realizado com sucesso!';
       console.log('Cadastro:', this.cadastro);
     } else {
+      this.modalMessage = 'Erro: Por favor, verifique os dados do formulário.';
       console.log('Formulário inválido');
     }
+    this.showModal = true; // Exibe o modal
+  }
+
+  closeModal() {
+    this.showModal = false; // Fecha o modal
   }
 }
